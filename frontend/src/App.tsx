424c6ed56fcc31700f4dde1fde77b93fd8235ebc
RamRagDevSecOps <v.sriram.sscet@gmail.com>
2026-03-10 16:42:13 +0530

    test commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-// Updated homepage layout - v1.1 - Learning CI/CD with Wanderlust
+// Updated test homepage layout - v1.1 - Learning CI/CD with Wanderlust
 // This comment triggers our Jenkins pipeline for demo purposes
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
